47d4d675f20813c6876e60793a9de2229c5f54be
stephenrey23 <stephenjreyes41@gmail.com>
2026-05-13 22:07:46 -0600

    Add files via upload

---
+++ sauceDemo.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+describe('Flujo completo de compra', () => {
+    beforeEach(function() {
+        cy.fixture('users').then((data) => {
+            this.userData = data; 
+            cy.visit('https://www.saucedemo.com/');
+            cy.get('#user-name').type(this.userData.usuarioValido);
+            cy.get('#password').type(this.userData.passwordValido);
+            cy.get('#login-button').click();
+        });
+    });
+
+    it('Compra de inicio a fin', function() {
+        cy.get('.title').should('contain', 'Products');
+        cy.get('.inventory_item_price').first().should('have.text', '$29.99');
+        cy.get('.btn.btn_primary.btn_small.btn_inventory').click();
+        cy.get('.shopping_cart_badge').should('have.text', '1');    
+        cy.get('.shopping_cart_link').click();
+        cy.get('#checkout').click();
+        cy.get('#first-name').type(this.userData.nombre); 
+        cy.get('#last-name').type(this.userData.apellido);
+        cy.get('#postal-code').type(this.userData.codigoPostal);
+        cy.get('#continue').click();
+        cy.get('#finish').click();
+        cy.get('.complete-header').should('contain', 'Thank you for your order!');
+    });
+});
